74d72bc1693e6bc7fa925acb734d535b7a6ddda3
akzin05 <aungkhantzin50@gmail.com>
2020-06-13 05:11:27 +0800

    commit 2 using moq,create entity and repo

---
+++ OnlineShoppingStoreWebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShoppingStore.Domain.Abstract;
+
+namespace OnlineShoppingStoreWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
