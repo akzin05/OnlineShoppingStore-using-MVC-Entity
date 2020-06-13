187239c85b51948d0db3115bead3f384b2ae2ffb
akzin05 <aungkhantzin50@gmail.com>
2020-06-14 05:34:55 +0800

    pagination

--- OnlineShoppingStoreWebUI/Controllers/ProductController.cs
+++ OnlineShoppingStoreWebUI/Controllers/ProductController.cs
@@ -4,20 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStoreWebUI.Models;
 
 namespace OnlineShoppingStoreWebUI.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductRepository repository;
- 
+        public int PageSize = 3;
+
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel()
+            {
+                Products = repository.Products
+                        .OrderBy(p => p.ProductId)
+                        .Skip((page - 1) * PageSize)
+                        .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ OnlineShoppingStoreWebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStoreWebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        
+
+    }
+}
