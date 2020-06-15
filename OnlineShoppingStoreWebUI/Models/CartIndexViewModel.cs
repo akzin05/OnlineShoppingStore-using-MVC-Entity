5fa1b8bac8b403f78444f185827e22da0e3cdee6
akzin05 <aungkhantzin50@gmail.com>
2020-06-15 18:31:49 +0800

    building Shopping Cart

---
+++ OnlineShoppingStoreWebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStoreWebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
